01ec7313647c5abe5a3a9ba01f6151b4cc945e94
Rupert Taylor <rupert.taylor1982@gmail.com>
2023-10-01 14:35:30 +0100

    module 4 starting code

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,54 @@
+import type { Actions, PageServerLoad } from "./$types";
+import { setError, superValidate } from "sveltekit-superforms/server";
+import { fail, redirect } from "@sveltejs/kit";
+import { registerUserSchema } from "$lib/schemas";
+
+
+export const load: PageServerLoad = async (event) => {
+
+  // Gets the session and if there is a valid session then 
+  // it redirects to the home page.
+  const session = await event.locals.getSession();
+  if (session) {
+    throw redirect(302, '/');
+  }
+
+  // loads the validation schema for the registration form
+  return {
+    form: superValidate(registerUserSchema),
+  };
+};
+
+export const actions: Actions = {
+  default: async (event) => {
+    const form = await superValidate(event, registerUserSchema);
+
+    if (!form.valid) {
+      return fail(400, {
+        form,
+      });
+    }
+
+    if (form.data.password !== form.data.passwordConfirm) {
+      return setError(form, "passwordConfirm", "Passwords do not match");
+    }
+
+    const { error: authError } = await event.locals.supabase.auth.signUp({
+      email: form.data.email,
+      password: form.data.password,
+      options: {
+        data: {
+          full_name: form.data.full_name ?? "",
+        },
+      },
+    });
+
+    if (authError) {
+      return setError(form, null, "An error occurred while registering.");
+    }
+
+    return {
+      form,
+    };
+  },
+};
